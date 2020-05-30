f0216e2e5df6413a6e7838192c3aeb27e0139ad1
bhaskar <b.bhaskar.nair@gmail.com>
2020-05-31 03:06:06 +0530

    exit table added

--- lib/components/isk_holder.dart
+++ lib/components/isk_holder.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:coup/modals/isk.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,31 +10,53 @@ class IskHolder extends StatelessWidget {
   Widget build(BuildContext context) {
     final isk = Provider.of<Isk>(context);
 
-    return Container(
-      padding: EdgeInsets.all(5),
-      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 0),
-      width: 70,
-      height: 35,
-      decoration: BoxDecoration(
-          color: Colors.blueGrey, borderRadius: BorderRadius.circular(20)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          Image.asset(
-            'assets/roleLogos/contessa.png',
-            width: 25,
-            height: 25,
+    final HttpsCallable leaveTable = CloudFunctions.instance.getHttpsCallable(
+      functionName: 'tableFunctions-leaveTable',
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(5),
+          margin: EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+          width: 70,
+          height: 35,
+          decoration: BoxDecoration(
+              color: Colors.blueGrey, borderRadius: BorderRadius.circular(20)),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Image.asset(
+                'assets/roleLogos/contessa.png',
+                width: 25,
+                height: 25,
+              ),
+              SizedBox(
+                width: 25,
+                child: Text(
+                  "${isk.counter}",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+              ),
+            ],
           ),
-          SizedBox(
-            width: 25,
-            child: Text(
-              "${isk.counter}",
-              textAlign: TextAlign.end,
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
+        ),
+        SizedBox(
+          height: 20,
+          width: 60,
+          child: FlatButton(
+            color: Colors.blue,
+            child: Text("Exit", style: TextStyle()),
+            onPressed: () async {
+              var resp = await leaveTable();
+              Navigator.of(context).pushReplacementNamed('/home-screen');
+            },
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
